77cb81e6cc7477b43c3c3bf77115ab31e7d084fa
0xBriz <chaindevone@gmail.com>
2022-08-04 08:25:39 -0400

    fix goerli addresses

--- src/mappings/helpers/constants.ts
+++ src/mappings/helpers/constants.ts
@@ -31,7 +31,7 @@ let network: string = dataSource.network();
 let vaultAddressByNetwork: AddressByNetwork = {
   mainnet: "0xc37c34eA9CA579aDF279A16C547e801ed722F3b5",
   kovan: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
-  goerli: "0x65748E8287Ce4B9E6D83EE853431958851550311",
+  goerli: "0x0Cc23b51B3A89728c85a63c819E8283e353FC86c",
   rinkeby: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
   polygon: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
   arbitrum: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
@@ -42,7 +42,7 @@ let vaultAddressByNetwork: AddressByNetwork = {
 let wethAddressByNetwork: AddressByNetwork = {
   mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB
   kovan: "0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
-  goerli: "0x9A1000D492d40bfccbc03f413A48F5B6516Ec0Fd",
+  goerli: "0xBd0be709AbE750641524fDECA1F49544440B7C0d",
   rinkeby: "0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
   polygon: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
   arbitrum: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
@@ -53,7 +53,7 @@ let wethAddressByNetwork: AddressByNetwork = {
 let wbtcAddressByNetwork: AddressByNetwork = {
   mainnet: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
   kovan: "0x1C8E3Bcb3378a443CC591f154c5CE0EBb4dA9648",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0xc44101175e573760F3d1c27396550F110efD88Cb",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
   arbitrum: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
@@ -75,7 +75,7 @@ let usdAddressByNetwork: AddressByNetwork = {
 let usdcAddressByNetwork: AddressByNetwork = {
   mainnet: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
   kovan: "0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0x1c0b84a4DF2bfeA7A2b4F30E33D6EA1CfEf8373B",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
   arbitrum: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
@@ -86,7 +86,7 @@ let usdcAddressByNetwork: AddressByNetwork = {
 let usdtAddressByNetwork: AddressByNetwork = {
   mainnet: "0x55d398326f99059fF775485246999027B3197955",
   kovan: "0xcC08220af469192C53295fDd34CFb8DF29aa17AB",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0x72a96214DBFfD17385e6eF68885df4fffFC0ADf3",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
   arbitrum: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
@@ -97,7 +97,7 @@ let usdtAddressByNetwork: AddressByNetwork = {
 let balAddressByNetwork: AddressByNetwork = {
   mainnet: "0x294e665AA26F63EC2d65345e69f99A73677be825", // TODO: Our token
   kovan: "0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0x3aB9D2bBDc2ce0412d7395a6c5F5fe5f27990b44",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3",
   arbitrum: "0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8",
@@ -108,7 +108,7 @@ let balAddressByNetwork: AddressByNetwork = {
 let daiAddressByNetwork: AddressByNetwork = {
   mainnet: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
   kovan: "0x04DF6e4121c27713ED22341E7c7Df330F56f289B",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0x72a96214DBFfD17385e6eF68885df4fffFC0ADf3",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
   arbitrum: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
