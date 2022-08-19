7fa611a6b319a8fa765ea107a8233ea4f6f9d5a8
0xBriz <chaindevone@gmail.com>
2022-08-19 16:15:12 -0400

    pricing

--- src/mappings/helpers/constants.ts
+++ src/mappings/helpers/constants.ts
@@ -42,7 +42,7 @@ let vaultAddressByNetwork: AddressByNetwork = {
 let wethAddressByNetwork: AddressByNetwork = {
   mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB
   kovan: "0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
-  goerli: "0xeFA07706B07934157c0dA4c6e91ac251bd36095a",
+  goerli: "0xeFA07706B07934157c0dA4c6e91ac251bd36095a", // Fake WBNB
   rinkeby: "0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
   polygon: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
   arbitrum: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
@@ -61,15 +61,26 @@ let wbtcAddressByNetwork: AddressByNetwork = {
   bsc: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
 };
 
-let usdAddressByNetwork: AddressByNetwork = {
-  mainnet: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d", // USDC
+// let usdAddressByNetwork: AddressByNetwork = {
+//   mainnet: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD
+//   kovan: "0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
+//   goerli: "0xCDCc86CfA84dc6808F87eE997d96C5F8eE5549F4", // Fake BUSD
+//   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
+//   polygon: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+//   arbitrum: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+//   dev: "0x1528f3fcc26d13f7079325fb78d9442607781c8c",
+//   bsc: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD
+// };
+
+let busdAddressByNetwork: AddressByNetwork = {
+  mainnet: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD
   kovan: "0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
-  goerli: "0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86",
+  goerli: "0xCDCc86CfA84dc6808F87eE997d96C5F8eE5549F4",
   rinkeby: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
   polygon: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
   arbitrum: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
-  dev: "0x1528f3fcc26d13f7079325fb78d9442607781c8c",
-  bsc: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+  dev: "0x7c0c5AdA758cf764EcD6bAC05b63b2482f90bBB2",
+  bsc: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD
 };
 
 let usdcAddressByNetwork: AddressByNetwork = {
@@ -116,6 +127,39 @@ let daiAddressByNetwork: AddressByNetwork = {
   bsc: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
 };
 
+let balBptAddressByNetwork: AddressByNetwork = {
+  mainnet: "0x7a09ddF458FdA6e324A97D1a8E4304856fb3e702",
+  kovan: "0x61d5dc44849c9c87b0856a2a311536205c96c7fd",
+  goerli: "0x3a103f8614a9616af0706a729949fec8a81df05b",
+  rinkeby: "0x0000000000000000000000000000000000000000",
+  polygon: "0x3d468AB2329F296e1b9d8476Bb54Dd77D8c2320f",
+  arbitrum: "0x0000000000000000000000000000000000000000",
+  dev: "0x0000000000000000000000000000000000000000",
+  bsc: "0x7a09ddF458FdA6e324A97D1a8E4304856fb3e702",
+};
+
+let amesAddressByNetwork: AddressByNetwork = {
+  mainnet: "0xb9E05B4C168B56F73940980aE6EF366354357009",
+  kovan: "0x61d5dc44849c9c87b0856a2a311536205c96c7fd",
+  goerli: "0x7B6C1CBa7040338079e25693287034589d04a6b6",
+  rinkeby: "0x0000000000000000000000000000000000000000",
+  polygon: "0x3d468AB2329F296e1b9d8476Bb54Dd77D8c2320f",
+  arbitrum: "0x0000000000000000000000000000000000000000",
+  dev: "0x0000000000000000000000000000000000000000",
+  bsc: "0xb9E05B4C168B56F73940980aE6EF366354357009",
+};
+
+let ethAddressByNetwork: AddressByNetwork = {
+  mainnet: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+  kovan: "0x61d5dc44849c9c87b0856a2a311536205c96c7fd",
+  goerli: "0xBd0be709AbE750641524fDECA1F49544440B7C0d",
+  rinkeby: "0x0000000000000000000000000000000000000000",
+  polygon: "0x3d468AB2329F296e1b9d8476Bb54Dd77D8c2320f",
+  arbitrum: "0x0000000000000000000000000000000000000000",
+  dev: "0x0000000000000000000000000000000000000000",
+  bsc: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+};
+
 function forNetwork(addressByNetwork: AddressByNetwork, network: string): Address {
   if (network == "mainnet") {
     return Address.fromString(addressByNetwork.mainnet);
@@ -137,14 +181,17 @@ function forNetwork(addressByNetwork: AddressByNetwork, network: string): Addres
 }
 
 export let VAULT_ADDRESS = forNetwork(vaultAddressByNetwork, network);
-export let WETH: Address = forNetwork(wethAddressByNetwork, network);
+export let WETH: Address = forNetwork(wethAddressByNetwork, network); // WBNB
 export let WBTC: Address = forNetwork(wbtcAddressByNetwork, network);
-export let USD: Address = forNetwork(usdAddressByNetwork, network);
+export let BUSD: Address = forNetwork(busdAddressByNetwork, network); // BUSD
 export let USDC: Address = forNetwork(usdcAddressByNetwork, network);
 export let USDT: Address = forNetwork(usdtAddressByNetwork, network);
-export let BAL: Address = forNetwork(balAddressByNetwork, network);
-export let DAI: Address = forNetwork(daiAddressByNetwork, network);
+export let AEQ: Address = forNetwork(balAddressByNetwork, network);
+export let AMES: Address = forNetwork(amesAddressByNetwork, network);
+export let ETH: Address = forNetwork(ethAddressByNetwork, network);
+
+export let AEQ_BPT: Address = forNetwork(balBptAddressByNetwork, network);
 
 // PRICING_ASSETS must be sorted by order of preference
-export let PRICING_ASSETS: Address[] = [USDC, DAI, USDT, BAL];
-export let USD_STABLE_ASSETS: Address[] = [USDC, DAI, USDT];
+export let PRICING_ASSETS: Address[] = [BUSD, WETH, WBTC, USDC, USDT, AEQ_BPT, AEQ, AMES, ETH];
+export let USD_STABLE_ASSETS: Address[] = [BUSD, USDC, USDT];
